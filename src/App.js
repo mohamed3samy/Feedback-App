dd525f5f8136cf2dec774ef0f7a798d65c8869a3
mohamed3samy <mohamed701samy@gmail.com>
2022-03-09 16:11:35 +0200

    Components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,30 @@
+import { useState } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Header from "./components/Header";
+import FeedbackData from "./data/FeedbackData";
+import FeedbackList from "./components/FeedbackList";
+import FeedbackStats from "./components/FeedbackStats";
+
 const App = () => {
+	const [feedback, setFeedback] = useState(FeedbackData);
+
+	const deleteFeedback = (id) => {
+		if (window.confirm("Are you sure you want to delete?")) {
+			setFeedback(feedback.filter((item) => item.id !== id));
+		}
+	};
+
 	return (
-		<div>
-			<div>App start</div>
-		</div>
+		<Router>
+			<Header />
+			<div className="container">
+				<FeedbackStats feedback={feedback} />
+				<FeedbackList
+					feedback={feedback}
+					handleDelete={deleteFeedback}
+				/>
+			</div>
+		</Router>
 	);
 };
 
